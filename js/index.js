07933b5344adcbdefdf588a5b2a612b71144db8b
Tech-A41 <Tech-A41@Tech-A41s-MacBook-Air.local>
2016-11-28 16:07:33 +0000

    added carousel

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
  $(function(){
    console.log('Load');
     $(".button-collapse").sideNav();
-    $('.carousel.carousel-slider').carousel({full_width: true});
     Materialize.updateTextFields();
+    $('.images').slick({
+      dots:true
+    });
  });
